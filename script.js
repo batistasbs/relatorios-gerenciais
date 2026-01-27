f7c7c9bd9547ca018713c63cab92e73189bd9f10
batistasbs <batistasbs@gmail.com>
2026-01-27 14:30:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+body {
+    font-family: Arial, sans-serif;
+    background: #f4f6f8;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+}
+
+#login {
+    margin-top: 100px;
+}
+
+input {
+    padding: 10px;
+    width: 250px;
+}
+
+button {
+    padding: 10px 20px;
+    margin-top: 10px;
+    cursor: pointer;
+}
+
+#erro {
+    color: red;
+}
+
+.hidden {
+    display: none;
+}
+
+iframe {
+    width: 95%;
+    height: 80vh;
+    border: none;
+    margin-top: 20px;
+}
 
