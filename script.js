765f7b2b2a6b595b6f21ee50967d3def91ac62d6
batistasbs <batistasbs@gmail.com>
2026-01-27 17:38:08 -0300

    Add orcamento_2026 to budget report structure

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const relatorios = {
         titulo: "Relatório de Faturamento",
         url: "https://app.powerbi.com/view?r=eyJrIjoiYjAwMmM0NzUtNmM3OS00OWIyLThhNTItMTg1OGExMzM5MTk5IiwidCI6ImJiNWFlODA1LTM1YmYtNGRkNS05NGZlLWQ4M2JiMWVkNDI0MiJ9"
     },
-    Orçamento: {
+    orcamento_2026: {
         titulo: "Orçamento 2026",
         url: "https://app.powerbi.com/view?r=eyJrIjoiNWQ0MmExYTktZmFkMi00MDc1LWExODEtYjMzNTg1Y2JiNTY5IiwidCI6ImJiNWFlODA1LTM1YmYtNGRkNS05NGZlLWQ4M2JiMWVkNDI0MiJ9"
     }
